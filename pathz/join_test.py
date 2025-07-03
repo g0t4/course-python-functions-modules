6194f6172fdb24a9f2398225b64dbb1fa6680344
Wes Higbee <wes.mcclure@gmail.com>
2025-07-03 14:53:10 -0500

    strip comments after reminder

--- pathz/join_test.py
+++ pathz/join_test.py
@@ -1,8 +1,4 @@
-# import os
 from os import path
-
-# inspiration:
-# repo_dir = os.path.expanduser(os.path.join("~/repos", host_name, parsed["repo_path"]))
 
 def test_joiner_no_tilde_expands_like_normal():
     joined = path.join("path", "to", "file.txt")
@@ -10,5 +6,4 @@ def test_joiner_no_tilde_expands_like_normal():
 
 def test_joiner_tilde_leading_expands_to_home_dir():
     joined = path.join("~/path", "to", "file.txt")
-    assert "/Users/wesdemos/path/to/file.txt" == joined  # Change to your HOME dir
-    # assert (os.getenv("HOME") or "") + "/path/to/file.txt" == joined  # OR, use this one that looks up HOME dir
+    assert "/Users/wesdemos/path/to/file.txt" == joined
