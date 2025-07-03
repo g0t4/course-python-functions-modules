feca43474ff9d7da60d9779e1d5cfb6960949eab
Wes Higbee <wes.mcclure@gmail.com>
2025-07-03 16:39:10 -0500

    rename to expanded_arg

--- pathz/__init__.py
+++ pathz/__init__.py
@@ -1,14 +1,14 @@
 import os
 
 def join(first_arg, *parts):
-    first_arg = os.path.expanduser(first_arg)
-    return os.path.join(first_arg, *parts)
+    expanded_arg = os.path.expanduser(first_arg)
+    return os.path.join(expanded_arg, *parts)
 
 def exists(first_arg):
-    first_arg = os.path.expanduser(first_arg)
-    return os.path.exists(first_arg)
+    expanded_arg = os.path.expanduser(first_arg)
+    return os.path.exists(expanded_arg)
 
 def abspath(first_arg):
-    first_arg = os.path.expanduser(first_arg)
-    return os.path.abspath(first_arg)
+    expanded_arg = os.path.expanduser(first_arg)
+    return os.path.abspath(expanded_arg)
 
