a0b2612e45a42a0bb85b83a594b38ea4b5b5a4e7
Wes Higbee <wes.mcclure@gmail.com>
2025-07-03 17:02:44 -0500

    port the other two too

--- pathz/__init__.py
+++ pathz/__init__.py
@@ -10,17 +10,19 @@ def new_func_factory(func):
 
 
 join = new_func_factory(os.path.join)
+abspath = new_func_factory(os.path.abspath)
+exists = new_func_factory(os.path.exists)
 
 #
 # def join(first_component, *parts):
 #     expanded_arg = os.path.expanduser(first_component)
 #     return os.path.join(expanded_arg, *parts)
-
-def exists(first_component):
-    expanded_arg = os.path.expanduser(first_component)
-    return os.path.exists(expanded_arg)
-
-def abspath(first_component):
-    expanded_arg = os.path.expanduser(first_component)
-    return os.path.abspath(expanded_arg)
-
+#
+# def exists(first_component):
+#     expanded_arg = os.path.expanduser(first_component)
+#     return os.path.exists(expanded_arg)
+#
+# def abspath(first_component):
+#     expanded_arg = os.path.expanduser(first_component)
+#     return os.path.abspath(expanded_arg)
+#
