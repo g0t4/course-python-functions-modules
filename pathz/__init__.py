b85145e701db924433e0499a6eb6a6e3f974ce7e
Wes Higbee <wes.mcclure@gmail.com>
2025-07-03 16:55:54 -0500

    extract a function "factory" that can make our new functions using the same template!

--- pathz/__init__.py
+++ pathz/__init__.py
@@ -1,8 +1,20 @@
 import os
 
-def join(first_component, *parts):
-    expanded_arg = os.path.expanduser(first_component)
-    return os.path.join(expanded_arg, *parts)
+def new_func_factory(func):
+
+    def new_func(first_component, *parts):
+        expanded_arg = os.path.expanduser(first_component)
+        return func(expanded_arg, *parts)
+
+    return new_func
+
+
+join = new_func_factory(os.path.join)
+
+#
+# def join(first_component, *parts):
+#     expanded_arg = os.path.expanduser(first_component)
+#     return os.path.join(expanded_arg, *parts)
 
 def exists(first_component):
     expanded_arg = os.path.expanduser(first_component)
