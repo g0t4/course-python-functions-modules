e23940b6c62e9c6165f2a32c5c59ae1381011b60
Wes Higbee <wes.mcclure@gmail.com>
2025-07-03 16:06:51 -0500

    add new tests

--- pathz/join_test.py
+++ pathz/join_test.py
@@ -8,3 +8,12 @@ def test_joiner_no_tilde_expands_like_normal():
 def test_joiner_tilde_leading_expands_to_home_dir():
     joined = path.join("~/path", "to", "file.txt")
     assert "/Users/wesdemos/path/to/file.txt" == joined
+
+def test_exists():
+    assert path.exists("~/repos")
+
+def test_abs():
+    abs_path = path.abspath("~/")
+    assert "/Users/wesdemos" == abs_path
+
+
