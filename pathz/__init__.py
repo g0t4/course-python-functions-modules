b07c53cb221cdbc56c6dbd644af0e0607a52d63b
Wes Higbee <wes.mcclure@gmail.com>
2025-07-03 16:07:11 -0500

    show manual variations

--- pathz/__init__.py
+++ pathz/__init__.py
@@ -3,4 +3,9 @@ import os
 def join(*parts):
     return os.path.expanduser(os.path.join(*parts))
 
+def exists(path):
+    return os.path.exists(os.path.expanduser(path))
+
+def abspath(path):
+    return os.path.abspath(os.path.expanduser(path))
 
