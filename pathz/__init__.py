1914f9025078b681b348e52c09089ef7f54fd43e
Wes Higbee <wes.mcclure@gmail.com>
2025-07-03 16:41:00 -0500

    rename again

--- pathz/__init__.py
+++ pathz/__init__.py
@@ -1,14 +1,14 @@
 import os
 
-def join(first_arg, *parts):
-    expanded_arg = os.path.expanduser(first_arg)
+def join(first_component, *parts):
+    expanded_arg = os.path.expanduser(first_component)
     return os.path.join(expanded_arg, *parts)
 
-def exists(first_arg):
-    expanded_arg = os.path.expanduser(first_arg)
+def exists(first_component):
+    expanded_arg = os.path.expanduser(first_component)
     return os.path.exists(expanded_arg)
 
-def abspath(first_arg):
-    expanded_arg = os.path.expanduser(first_arg)
+def abspath(first_component):
+    expanded_arg = os.path.expanduser(first_component)
     return os.path.abspath(expanded_arg)
 
